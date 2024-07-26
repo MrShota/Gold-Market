c6be3ae13cf4eb995de2f044d137e65d403c5010
Shota <shot.shavadze@gmail.com>
2024-07-26 14:07:16 +0400

    trying style carousel

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -18,12 +18,12 @@ export class GalleryComponent {
     { src: "../../assets/images/71Lmincsf7L._UL1500_.jpg" },
     { src: "../../assets/images/1644248918-0400014091286_GOLD.jpg" },
 
-    { src: "../../assets/images/47743ob_800.jpg" },
-    { src: "../../assets/images/barandringtoggle-clasps-category(16).jpg" },
-    { src: "../../assets/images/ring-concierge-bracelets-14k-yellow-gold-graduated-single-prong-diamond-bracelet-30024294858840.jpg.webp" },
-    { src: "../../assets/images/best-simple-gold-jewelry-293078-1620409827035-main.700x0c.jpg" },
-    { src: "../../assets/images/a744c114c6a6827ffc3794b2dd9a906b87f4640b.jpg" },
-    { src: "../../assets/images/Jewellery-1.jpg" }
+  //   { src: "../../assets/images/47743ob_800.jpg" },
+  //   { src: "../../assets/images/barandringtoggle-clasps-category(16).jpg" },
+  //   { src: "../../assets/images/ring-concierge-bracelets-14k-yellow-gold-graduated-single-prong-diamond-bracelet-30024294858840.jpg.webp" },
+  //   { src: "../../assets/images/best-simple-gold-jewelry-293078-1620409827035-main.700x0c.jpg" },
+  //   { src: "../../assets/images/a744c114c6a6827ffc3794b2dd9a906b87f4640b.jpg" },
+  //   { src: "../../assets/images/Jewellery-1.jpg" }
   ]
 
 }
